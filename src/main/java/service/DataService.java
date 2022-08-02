c6066612992330a73217ff0ee2103faf870f5480
Liam Wartig <liam.wartig@olmsystems.com>
2022-08-02 15:11:45 +0100

    saves student data after exit

--- src/main/java/service/DataService.java
+++ src/main/java/service/DataService.java
@@ -1,5 +1,6 @@
 package service;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -16,7 +17,7 @@ public class DataService {
   
   public static void saveData() throws Exception {
     try {
-      File dataFile = new File("UniversityRegistrationData.txt");
+      File dataFile = new File("UniversityRegistrationData.csv");
       if (dataFile.createNewFile()) {
         System.out.println("File created: " + dataFile.getName());
       } else {
@@ -27,24 +28,35 @@ public class DataService {
     }
     
     try {
-      FileWriter writer = new FileWriter("UniversityRegistrationData.txt");
-      String data = formatData();
-      writer.write(data);
-      writer.close();
+      writeStudentData();
       System.out.println("Successfully wrote to the file.");
     } catch (IOException e) {
       System.out.println("An error occurred writing data to datafile.");
     }
   }
   
-  private static String formatData() throws Exception{
-    String data = "";
-    for (int i=1; i==Utils.studentIdCounter; i++) {
+  private static void writeStudentData() throws Exception{
+    BufferedWriter writer = new BufferedWriter(new FileWriter("UniversityRegistrationData.csv"));
+    writer.write("");
+    writer.write("studentName,studentAge,assignedCourses");
+    writer.newLine();
+    for (int i=1; i<Utils.studentIdCounter+1; i++) {
       Student student = Utils.getStudent(i);
-      data = data + (student.getName() + "," + student.getAge() + "," + student.getAssignedCourseIds().toString() + "\n");
-    
+      String studentName = student.getName();
+      String studentAge = String.valueOf(student.getAge());
+      String assignedCourses = student.assignedCourseIds.toString();
+       
+      if (assignedCourses == null) {
+          assignedCourses = "[]";   // write empty value for null
+      }
+      
+      String line = String.format("%s,%s,%s",
+              studentName, studentAge, assignedCourses);
+       
+      writer.write(line);
+      writer.newLine();
     }
-    return data;
+    writer.close();
   }
   
 }
